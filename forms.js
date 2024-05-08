37e0076fae6db2745713f65edda61142f9d60aa6
Fertilesoil <gasnbloop@gmail.com>
2024-05-08 13:41:12 -0300

    Refatoração da lógica do forms e estilos relacionados

--- forms.js
+++ forms.js
@@ -1,20 +1,30 @@
 ﻿import { init, send } from "@emailjs/browser";
 
 const nome = document.querySelector(".nome__forms");
-const legendNome = document.querySelector(".nome__forms__legend");
 const email = document.querySelector(".email__forms");
-const legendEmail = document.querySelector(".email__forms__legend");
 const assunto = document.querySelector(".assunto__forms");
-const legendAssunto = document.querySelector(".assunto__forms__legend");
 const mensagem = document.querySelector("#mensagem");
 const botao = document.querySelector(".contato__botao");
-const inputs = [...document.querySelectorAll("[data-input]")]
+const inputs = document.querySelectorAll('.contato__forms input, #mensagem');
+const legends = document.querySelectorAll('.contato__forms input');
 
-inputs.onchange = () => {
-  
-};
+botao.classList.add("disabled");
 
-// botao.classList.add("disabled");
+inputs.forEach(input => {
+  input.addEventListener('input', verificarCamposPreenchidos);
+});
+
+function verificarCamposPreenchidos() {
+  const inputsPreenchidos = [...inputs].every(input => input.value.trim() !== '');
+
+  if (inputsPreenchidos) {
+    botao.classList.remove("disabled");
+    botao.classList.add("enabled");
+  } else {
+    botao.classList.remove("enabled");
+    botao.classList.add("disabled");
+  }
+}
 
 const barrarEscrita = (e) => {
   const elemento = e.currentTarget;
@@ -36,13 +46,11 @@ const validacao = (elemento, vazio, excedente) => {
   if (input === "") {
     span.textContent = `${vazio}`;
     span.style.display = "flex";
-    // botao.classList.add("disabled");
   }
 
   if (input.length > 50) {
     span.textContent = `${excedente}`;
     span.style.display = "flex";
-    // botao.classList.add("disabled");
   }
 
   elemento.addEventListener("keypress", (e) => barrarEscrita(e));
@@ -54,11 +62,6 @@ nome.addEventListener("input", (e) => {
     nome,
     "* Campo nome não pode estar vazio.",
     "* Nome não pode ter mais que 50 caracteres.");
-  if (nome.value.length > 0) {
-    botao.classList.remove("disabled");
-  } else if (nome.value.length === 0) {
-    botao.classList.add("disabled");
-  }
 });
 
 email.addEventListener("input", (e) => {
@@ -67,11 +70,6 @@ email.addEventListener("input", (e) => {
     email,
     "* Email não pode estar vazio.",
     "* Email não pode ter mais que 50 caracteres.");
-  if (email.value.length > 0) {
-    botao.classList.remove("disabled");
-  } else if (email.value.length === 0) {
-    botao.classList.add("disabled");
-  }
 });
 
 assunto.addEventListener("input", (e) => {
@@ -80,55 +78,19 @@ assunto.addEventListener("input", (e) => {
     assunto,
     "* Assunto não pode estar vazio.",
     "* Assunto não pode ter mais que 50 caracteres.");
-  if (assunto.value.length > 0) {
-    botao.classList.remove("disabled");
-  } else if (assunto.value.length === 0) {
-    botao.classList.add("disabled");
-  }
 });
 
-mensagem.addEventListener("input", (e) => {
-  guardarValores(e)
-  if (mensagem.value.length > 0) {
-    botao.classList.remove("disabled");
-  } else if (mensagem.value.length === 0) {
-    botao.classList.add("disabled");
-  }
+mensagem.addEventListener("input", (e) => guardarValores(e));
+
+legends.forEach(legend => {
+  legend.addEventListener('focus', () => {
+    legend.previousElementSibling.style.color = "rgba(15, 67, 145, 1)";
+  });
+
+  legend.addEventListener('blur', () => {
+    legend.previousElementSibling.style.color = "gray";
+  });
 });
-
-// console.log(elementos);
-
-// const validacaoBotao = nome.value.length === 0 && email.value.length === 0 && assunto.value.length === 0 && mensagem.value.length === 0;
-
-// if (validacaoBotao) {
-//   botao.classList.add("disabled");
-// } else if (!validacaoBotao) {
-//   botao.classList.remove("disabled");
-// }
-
-nome.onfocus = (e) => {
-  legendNome.style.color = "rgba(15, 67, 145, 1)";
-}
-
-nome.onblur = () => {
-  legendNome.style.color = "gray";
-}
-
-email.onfocus = () => {
-  legendEmail.style.color = "rgba(15, 67, 145, 1)";
-}
-
-email.onblur = () => {
-  legendEmail.style.color = "gray";
-}
-
-assunto.onfocus = () => {
-  legendAssunto.style.color = "rgba(15, 67, 145, 1)";
-}
-
-assunto.onblur = () => {
-  legendAssunto.style.color = "gray";
-}
 
 const options = {
   publicKey: import.meta.env.VITE_PUBLIC_KEY,
